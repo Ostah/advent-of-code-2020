fe28a17f729dabbdd5acb2f2348d7f3a70ddfaa9
Michal Ostaszewski <m.ostaszewski@ocado.com>
2020-12-03 17:43:48 +0100

    Day template

---
+++ advent/src/main/java/com/ostach/advent/dayX/DayX.java
@@ -0,0 +1,11 @@
+package com.ostach.advent.dayX;
+
+
+class DayX {
+
+    public void run1() {
+    }
+    public void run2() {
+    }
+
+}
---
+++ advent/src/main/java/com/ostach/advent/dayX/DayXRunner.java
@@ -0,0 +1,12 @@
+package com.ostach.advent.dayX;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class DayXRunner {
+
+    public static void main(String[] args) {
+        new DayX().run1();
+        new DayX().run2();
+    }
+}
